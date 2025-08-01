d63865963ee1d4f118685d6ebdc86c322aa4e197
Rafael Maranduba de Almeida <rmalmeidax@gmail.com>
2025-08-01 12:16:50 -0300

    first commit

---
+++ SistemaMercado/src/main/java/com/rmalmeidax/SistemaMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.rmalmeidax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMercadoApplication.class, args);
+	}
+
+}
---
+++ SistemaMercado/src/test/java/com/rmalmeidax/SistemaMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmalmeidax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
